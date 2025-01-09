54d72ee97f1c0550e001b23db7cfd8677a873225
LuckyBoy721 <139191581+LuckyBoy721@users.noreply.github.com>
2025-01-09 13:35:06 +0700

    Update new.py

--- new.py
+++ new.py
@@ -89,9 +89,9 @@ def display_accuracy_chart(accuracies):
     scores = list(accuracies.values())
     # fig.patch.set_facecolor('#1e1e21')  
     ax.set_facecolor('#1e1e21')
-    ax.barh(model_names, scores, color='orange')
-    ax.set_xlabel('Accuracy', color='white')
-    ax.set_title('Model Accuracy Comparison', color='white')
+    ax.barh(model_names, scores, color='gold')
+    ax.set_xlabel('Accuracy', color='black')
+    ax.set_title('Model Accuracy Comparison', color='black')
     st.pyplot(fig)
 
 
