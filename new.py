2e8dd5a9c3aecd6162a414c7df7e0d47eae2f293
LuckyBoy721 <139191581+LuckyBoy721@users.noreply.github.com>
2025-01-09 12:26:05 +0700

    Update new.py

--- new.py
+++ new.py
@@ -140,7 +140,7 @@ def main():
 
     # Sidebar navigation
     st.sidebar.title("Navigation")
-    set_background(b)
+    st.sidebar.image("b.jpg)
     page = st.sidebar.radio("Go to", ["Main Menu", "Gender Prediction", "Model Accuracies"])
 
     if page == "Main Menu":
